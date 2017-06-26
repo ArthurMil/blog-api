e4dc73046969ea1161b78dfb2372ce40f9a9f606
Artūras <arturas.milius@koralis.lt>
2017-06-27 00:11:56 +0300

    making posts + json connections with frontend

---
+++ app/Quote.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Quote extends Model
+{
+//    const A_CONTENT = 'content';
+//
+//    public function setContent($content) {
+//        $this->setAttribute(self::A_CONTENT, $content);
+//    }
+//
+//    public function getContent() {
+//        return $this->attributes[self::A_CONTENT];
+//    }
+}
